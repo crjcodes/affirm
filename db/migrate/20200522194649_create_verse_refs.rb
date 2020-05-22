2388b815dfab69bfb3a77c35982ce40fd5e4893b
crjcodes <crjohnson@codeonward.com>
2020-05-22 14:56:00 -0500

    database setup and seeding

--- db/migrate/20200522194649_create_verse_refs.rb
+++ db/migrate/20200522194649_create_verse_refs.rb
@@ -1,8 +1,8 @@
 class CreateVerseRefs < ActiveRecord::Migration[6.0]
   def change
     create_table :verse_refs do |t|
-      t.string :ref
-      t.Keyword :belongs_to
+      t.string :verse_ref
+      t.belongs_to :keyword, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,41 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+VerseRef.destroy_all
+Keyword.destroy_all
+
+keywords_to_seed = {
+  "anxiety"=>[ "Proverbs 3:26" ],
+  "community"=>[ "1 John 1:7", "Hebrews 13:1-2", "1 Peter 4:9", "Hebrews 10:24-25", "1 Thessalonians 5:14", "Matthew 18:20"],
+  "encouragement"=>[ "Proverbs 3:26", "Psalm 51:10", "Hebrews 13:15" ],
+  "fear"=>[ "Isaiah 41:10" ],
+  "endurance"=>[ "Psalm 51:10"],
+  "giving"=>["2 Corinthians 9:6-7"],
+  "loneliness"=>[ "Hebrews 13:15" ],
+  "thought"=>[ "2 Timothy 2:7" ]
+}
+
+
+keywords_to_seed.each do |kw, refs|
+
+  # COW: TO-DO: error handling for invalid keyword returned
+  # from either call
+
+  if not Keyword.exists?(keyword: kw)
+    a_keyword = Keyword.create(keyword: kw)
+  else
+    a_keyword = Keyword.find_by(keyword: kw)
+  end
+
+  refs.each do | bible_ref |
+    vr = VerseRef.create(verse_ref: bible_ref, keyword_id: a_keyword.id)
+  end
+end
+
+
+p "Created #{Keyword.count} affirm keywords"
+p "Created #{VerseRef.count} affirm verse references"
+
+
+
+
